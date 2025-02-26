9303d1226795a985288c22c293c5d7ad09dfc883
ChrisVai <chrisbellet@gmail.com>
2025-02-26 09:55:20 +0100

    Init Project

---
+++ src/main/java/fr/apply/applyapispring/ApplyApiSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.apply.applyapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyApiSpringApplication.class, args);
+    }
+
+}
